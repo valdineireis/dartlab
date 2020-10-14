42cd04a81bacc95dae69d103cc2cdbf523635c30
Valdinei Reis <valdineimtz@gmail.com>
2020-10-14 09:17:51 -0300

    Exemplo de lançamento de exceção

--- tratar_erro/app.dart
+++ tratar_erro/app.dart
@@ -1,8 +1,13 @@
 main() {
-  int x = 24;
+  int x = 0;
   int y = 0;
 
   try {
+    if (x <= 0) {
+      throw new FormatException('x is <= 0');
+    }
+
+
     var result = x ~/ y;
     print(result);
   } catch (e) {
