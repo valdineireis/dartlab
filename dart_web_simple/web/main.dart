a335316832bf2d070c6af84f14a247f2ad44f5cb
Valdinei Reis <valdineimtz@gmail.com>
2020-10-14 15:04:24 -0300

    Utilizando seletores html

--- dart_web_simple/web/main.dart
+++ dart_web_simple/web/main.dart
@@ -2,4 +2,17 @@ import 'dart:html';
 
 void main() {
   querySelector('#output').text = 'Your Dart app is running.';
+
+  var htmlLists = querySelectorAll('ul li');
+
+  var items = [
+    'Item 01',
+    'Item 02',
+    'Item 03',
+    'Item 04',
+  ];
+
+  for (var i = 0; i < htmlLists.length; i++) {
+    htmlLists[i].text = items[i];
+  }
 }
